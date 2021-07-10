87bb7ac0291a6d26dc7b928fede108b4b4d678fa
LeftyDark <rysmccar@ucsc.edu>
2021-07-10 12:18:27 -0700

    made placeholder card image less massive

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -4,7 +4,7 @@ class Play extends Phaser.Scene {
     }
     init() {}
     preload() {
-        this.load.image('card', './assets/temp_card.png');
+        this.load.image('card', './assets/temp_card_30.png');
     }
     create() {
         cardDeck = [];
