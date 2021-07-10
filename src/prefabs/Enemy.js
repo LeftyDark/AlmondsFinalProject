6ba95d4286852668f82e7e004a298d2a35896015
mjnakaji <mjnakaji@ucsc.edu>
2021-07-09 17:30:16 -0700

    created Enemy.js and Player.js

---
+++ src/prefabs/Enemy.js
@@ -0,0 +1,22 @@
+class Enemy extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // add to scene
+        scene.add.existing(this);
+        scene.physics.add.existing(this, 0);
+        //States the player can be in
+        
+        // add sfx here this.sfx<name> = scene.sound.add('sfx_<soundname>');
+        // make sure the file is already loaded in menu.js
+    }
+    create() {
+        // define key values 
+    }
+
+    update() {
+        
+    }
+        
+    reset() {}
+}
